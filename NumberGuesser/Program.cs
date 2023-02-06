5dae8dc567feeca303279f3ab194572941ee9375
Ignacio687 <ignadavidchaves@gmail.com>
2023-02-06 07:14:39 -0300

    firstPracticeFile

---
+++ NumberGuesser/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NumberGuesser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string name = input;
+            int age = 20;
+
+            Console.WriteLine("Hello {0}, your age is {1}",name,age);
+        }
+    }
+}
